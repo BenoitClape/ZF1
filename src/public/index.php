4e7d70062d8056327b495961e957d251f2a30acf
Administrateur <Administrateur@192.168.2.123>
2014-10-06 12:58:11 +0200

    mise en place de la base : index et error

---
+++ src/public/index.php
@@ -0,0 +1,15 @@
+<?php
+//echo 'toto';
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(dirname(__DIR__)));
+define('SRC_PATH', ROOT_PATH . DS . 'src' );
+define('PUBLIC_PATH', SRC_PATH . DS . 'public' );
+define('APP_PATH', SRC_PATH . DS . 'application' );
+define('CONFIG_PATH', APP_PATH . DS . 'configs' );
+//define('SRC_PATH', ROOT_PATH . DS . 'src' );
+
+require_once ROOT_PATH . DS . 'vendor' . DS . 'autoload.php';
+
+$application = new Zend_Application(getenv('APPLICATION_ENV') ?:'production', CONFIG_PATH . DS . 'application.ini');
+$application->bootstrap();
+$application->run();
