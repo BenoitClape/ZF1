873c954778a54e726bc0c83080d002c7648379e1
Administrateur <Administrateur@192.168.2.123>
2014-10-06 14:17:17 +0200

    correction

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,5 @@
 <?php
-//echo 'toto';
+echo getenv('APPLICATION_ENV');
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', dirname(dirname(__DIR__)));
 define('SRC_PATH', ROOT_PATH . DS . 'src' );
@@ -10,6 +10,6 @@ define('CONFIG_PATH', APP_PATH . DS . 'configs' );
 
 require_once ROOT_PATH . DS . 'vendor' . DS . 'autoload.php';
 
-$application = new Zend_Application(getenv('APPLICATION_ENV') ?:'production', CONFIG_PATH . DS . 'application.ini');
+$application = new Zend_Application(getenv('APPLICATION_ENV') ?: 'production', CONFIG_PATH . DS . 'application.ini');
 $application->bootstrap();
 $application->run();
